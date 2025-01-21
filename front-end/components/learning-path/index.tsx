3347d64cfdcad3adf745fe57dd724b797f52837a
hakan1368 <mehakan.10@gmail.com>
2025-01-21 03:31:49 +0100

    learning path logic updated

--- front-end/components/learning-path/index.tsx
+++ front-end/components/learning-path/index.tsx
@@ -7,15 +7,29 @@ type Props = {
 };
 
 const LearningPath: React.FC<Props> = ({ teacherId, learningPath }: Props) => {
-  const handleLearningPathChange = (event: { target: { value: string } }) => {
-    {
-      /* Use TeacherService to update the learning path for the teacher */
+  const [selectedLearningPath, setSelectedLearningPath] =
+    useState<string>(learningPath);
+  const handleLearningPathChange = async (event: {
+    target: { value: string };
+  }) => {
+    const newLearningPath = event.target.value;
+    setSelectedLearningPath(newLearningPath);
+    /* Use TeacherService to update the learning path for the teacher */
+    try {
+      await TeacherService.updateLearningPath(teacherId, newLearningPath);
+    } catch (error) {
+      throw new Error('Error updating the learning path.');
     }
   };
 
   return (
     <div className="ml-6">
-      <select id="learningPath" className="ml-2 p-1" value={''}>
+      <select
+        id="learningPath"
+        className="ml-2 p-1"
+        value={selectedLearningPath}
+        onChange={handleLearningPathChange}
+      >
         <option value="Infrastructure">Infrastructure</option>
         <option value="Software development">Software development</option>
         <option value="Cybersecurity">Cybersecurity</option>
